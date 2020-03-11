5fbd4318e58809faae8ef09009398c4b9244abbf
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-03-11 21:09:11 +1100

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+
+        System.out.println("Test - test klasy SimpleUser");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        if (Calculator.add(4, 19) == 23) {
+            System.out.println("Calculator.add() test OK");
+        } else {
+            System.out.println("Calculator.add() Error");
+        }
+
+        if (Calculator.subtract(4, 19) == -15) {
+            System.out.println("Calculator.subtract() test OK");
+        } else {
+            System.out.println("Calculator.subtract() Error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static double add(int a, int b) {
+        return a + b;
+    }
+
+    public static double subtract(int a, int b) {
+        return a - b;
+    }
+}
