55ac1d48c9df151d2bad9c1f031d3e79299f9684
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-03-15 17:16:07 +1100

    Modul 6.5; TDD done; ForumStatistics methods implemented; all tests green

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -9,10 +9,37 @@ public class ForumStatistics {
     private double avgCommentsPerPost;
 
     public void calculateAdvStatistics(Statistics statistics) {
+        this.usersCount = statistics.userNames().size();
+        this.postsCount = statistics.postsCount();
+        this.commentsCount = statistics.commentsCount();
 
+        if (this.usersCount > 0) {
+            this.avgPostsPerUser = (double)this.postsCount / (double)this.usersCount;
+        } else {
+            this.avgPostsPerUser = 0.0;
+        }
+
+        if (this.usersCount > 0) {
+            this.avgCommentsPerUser = (double)this.commentsCount / (double)this.usersCount;
+        } else {
+            this.avgCommentsPerUser = 0.0;
+        }
+
+        if (this.postsCount > 0) {
+            this.avgCommentsPerPost = (double)this.commentsCount / (double)this.postsCount;
+        } else {
+            this.avgCommentsPerPost = 0.0;
+        }
     }
 
     public void showStatistics() {
+        System.out.println("Forum statistics:");
+        System.out.println("Users count: " + this.usersCount);
+        System.out.println("Posts count: " + this.postsCount);
+        System.out.println("Comments count: " + this.commentsCount);
+        System.out.println("Average posts per user: " + this.avgPostsPerUser);
+        System.out.println("Average comments per user: " + this.avgCommentsPerUser);
+        System.out.println("Average comments per posts: " + this.avgCommentsPerPost);
     }
 
     public int getUsersCount() {
