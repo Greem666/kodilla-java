558abe9501cfe2cc0edf3c24892337763ab48517
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-05-02 23:21:28 +1000

    Exercise 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,36 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+
+public class MovieStore {
+
+    public static void main(String[] args) {
+
+        new MovieStore().getMovies().entrySet().stream()
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
+                .forEach(e -> System.out.print(e + "!"));
+    }
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+}
